32df48563ad1494e6363c27b0ecf011cfc296b54
jwiedeman <vielodesign@gmail.com>
2020-01-30 08:52:29 -0800

    Initial commit

---
+++ client/src/components/IndexComponent.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+      <h1>Posts</h1>
+        <div class="row">
+          <div class="col-md-10"></div>
+          <div class="col-md-2">
+            <router-link :to="{ name: 'create' }" class="btn btn-primary">Create Post</router-link>
+          </div>
+        </div><br />
+        <button v-on:click="log">Log post</button>
+        <table class="table table-hover">
+            <thead>
+            <tr>
+              <th>Title</th>
+              <th>Body</th>
+              <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="post in posts" :key="post._id">
+                  
+                  <td>{{ post.title }}</td>
+                  <td>{{ post.body }}</td>
+                  <td><router-link :to="{name: 'edit', params: { id: post._id }}" class="btn btn-primary">Edit</router-link></td>
+                  <td><button class="btn btn-danger" @click.prevent="deletePost(post._id)">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
+  </div>
+</template>
+
+<script>
+  export default {
+      data() {
+        return {
+          posts: []
+        }
+      },
+      created() {
+      let uri = 'http://localhost:4000/posts';
+      this.axios.get(uri).then(response => {
+        this.posts = response.data;
+      });
+    },
+    methods: {
+      log(){
+        console.log(this.posts)
+      },
+      deletePost(id)
+      {console.log('## TEST BEFORE' , this.posts.forEach(function(post){console.log(post.title)}))
+        let uri = `http://localhost:4000/posts/delete/${id}`;
+        this.axios.delete(uri).then(response => {
+          console.log('### ' ,  this.posts.splice(this.posts.indexOf(id), 1))
+          this.posts.splice(this.posts.indexOf(id), 1);
+          console.log('## TEST AFTER' , this.posts.forEach(function(post){console.log(post.title)}))
+          let uri = 'http://localhost:4000/posts';
+          this.axios.get(uri).then(response => {
+            this.posts = response.data;
+          });
+        });
+      }
+    }
+  }
+</script>
